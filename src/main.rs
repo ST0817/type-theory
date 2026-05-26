e9bdf3538381aa132935b008d2a78616c94eb844
ST0817 <136716138+ST0817@users.noreply.github.com>
2026-05-26 17:29:08 +0900

    added simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+use rustyline::{DefaultEditor, error::ReadlineError};
+
+fn main() -> ExitCode {
+    let mut editor = DefaultEditor::new().unwrap();
+
+    loop {
+        match editor.readline("❯ ") {
+            Ok(input) if !input.is_empty() => {
+                editor.add_history_entry(&input).unwrap();
+                println!("input: {input}");
+            }
+            Ok(_) => {}
+            Err(ReadlineError::Eof) => {
+                println!("Finished");
+                break ExitCode::SUCCESS;
+            }
+            Err(error) => {
+                eprintln!("Error: {error}");
+                break ExitCode::FAILURE;
+            }
+        }
+    }
 }
